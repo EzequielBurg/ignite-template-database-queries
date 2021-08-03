1b94a6c6583b09a285ce462f5947f577ca729fb3
EzequielBurg <ezekiel_sci@hotmail.com>
2021-08-02 22:59:57 -0300

    finishing challenge

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -14,17 +14,22 @@ export class GamesRepository implements IGamesRepository {
 
   async findByTitleContaining(param: string): Promise<Game[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder('game')
+      .where('LOWER(game.title) ILIKE LOWER(:title)', { title: `%${param}%` })
+      .getMany();
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT COUNT(*) FROM users`);
   }
 
-  async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+  async findUsersByGameId(id: string): Promise<User[] | undefined> {
+    const game = await this.repository
+      .createQueryBuilder('game')
+      .leftJoinAndSelect('game.users', 'users')
+      .where('game.id = :id', { id })
+      .getOne();
+
+    return game?.users;
   }
 }
